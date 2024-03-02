12e54c05ecf07c2e33f8098bdcd27c24b8fd8959
Hígor Sovinski <higorsovinski@gmail.com>
2024-03-02 00:24:05 -0300

    Configurações do Docker

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
